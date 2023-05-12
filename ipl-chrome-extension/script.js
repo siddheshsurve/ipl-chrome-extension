369403b3ac268e39d284dd47ed7311b8d135eb49
Siddhesh Surve <sysurve24@gmail.com>
2023-05-12 14:13:20 +0000

    added filter for ipl matches only by series_id

--- ipl-chrome-extension/script.js
+++ ipl-chrome-extension/script.js
@@ -10,7 +10,7 @@ async function getMatchData() {
 
       if (!matchesList) return [];
 
-      const relevantData = matchesList.map(
+      const relevantData = matchesList.filter(match => match.series_id == "c75f8952-74d4-416f-b7b4-7da4b4e3ae6e").map(
         (match) => `${match.name}, ${match.status}`
       );
       console.log(relevantData);
